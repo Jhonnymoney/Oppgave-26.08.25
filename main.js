9f13144e7eb238c9f9ff9c879beb91762da3c931
Jhonnymoney <jhonatansjohnsen@hotmail.com>
2025-08-26 13:09:04 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let message = document.getElementById("message");
+
+document.querySelector("#myBtn").addEventListener("click", () => {
+  const container = document.querySelector("#container");
+  container.innerHTML =
+    '<img src="./images/Cat.jpg" alt="Cat picure" style="width: 200px;">';
+});
+//
+document.addEventListener("DOMContentLoaded", () => {
+  message.textContent = "Click the button for a picture of a cat";
+});
+// Sett standardtekst
